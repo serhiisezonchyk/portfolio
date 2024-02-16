2852ea16df6c0be293c6418cd5f621920bc6f2a3
serhiisezonchyk <sergey040901@gmail.com>
2024-02-16 19:46:48 +0200

    prepare for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-    output:'export',
-    distDir:'out',
+    basePath: "/2048-in-react",
+    output: 'export',
+    distDir :'dist',
     images:{
         unoptimized:true
     }
