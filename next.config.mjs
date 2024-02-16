f246486874ff966fe5d9fb44c963fdd5fc159c20
serhiisezonchyk <sergey040901@gmail.com>
2024-02-16 19:52:02 +0200

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-    basePath: "/2048-in-react",
-    output: 'export',
-    distDir :'dist',
-    images:{
-        unoptimized:true
-    }
+  basePath: isProd ? '/portfolio' : '',
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
